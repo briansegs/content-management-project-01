a6d4786ad4ee0a2bf7a7ceb7d331752b169b362d
bsegers <briansegs@gmail.com>
2025-01-19 17:06:26 -0500

    feat: Setup theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 const Navbar = async () => {
   const session = await auth();
 
-  await console.log(session);
-
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
