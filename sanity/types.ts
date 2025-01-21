0ca59696444e113229d025a36862d2db48890b7e
bsegers <briansegs@gmail.com>
2025-01-21 16:46:54 -0500

    feat: Add startup query

--- sanity/types.ts
+++ sanity/types.ts
@@ -170,3 +170,30 @@ export type Markdown = string;
 
 export type AllSanitySchemaTypes = SanityImagePaletteSwatch | SanityImagePalette | SanityImageDimensions | SanityImageHotspot | SanityImageCrop | SanityFileAsset | SanityImageAsset | SanityImageMetadata | Geopoint | SanityAssetSourceData | Startup | Slug | Author | Markdown;
 export declare const internalGroqTypeReferenceTo: unique symbol;
+// Source: sanity/lib/queries.ts
+// Variable: STARTUPS_QUERY
+// Query: *[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {        _id,         title,         slug,         _createdAt,         author -> {          _id, name, image, bio        },         views,        description,        category,         image      }
+export type STARTUPS_QUERYResult = Array<{
+  _id: string;
+  title: string | null;
+  slug: Slug | null;
+  _createdAt: string;
+  author: {
+    _id: string;
+    name: string | null;
+    image: string | null;
+    bio: string | null;
+  } | null;
+  views: number | null;
+  description: string | null;
+  category: string | null;
+  image: string | null;
+}>;
+
+// Query TypeMap
+import "@sanity/client";
+declare module "@sanity/client" {
+  interface SanityQueries {
+    "*[_type == \"startup\" && defined(slug.current)] | order(_createdAt desc) {\n        _id, \n        title, \n        slug, \n        _createdAt, \n        author -> {\n          _id, name, image, bio\n        }, \n        views,\n        description,\n        category, \n        image\n      }": STARTUPS_QUERYResult;
+  }
+}
