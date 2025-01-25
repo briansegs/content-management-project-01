a96b1fc2f4d758be0b91293fa6e93c07d4bded20
bsegers <briansegs@gmail.com>
2025-01-25 11:34:13 -0500

    feat: Add user image to startup card

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -49,8 +49,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
-            alt="placeholder"
+            src={author?.image}
+            alt={author?.name}
             width={48}
             height={48}
             className="rounded-full"
